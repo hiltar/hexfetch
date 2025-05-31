7a0880ca1ae59affbabe04cfafdaad5179d8cf22
hiltar <43303122+hiltar@users.noreply.github.com>
2025-05-31 17:22:53 +0300

    Changes for fileserver

--- main.go
+++ main.go
@@ -694,7 +694,7 @@ func main() {
     startSystemInfoUpdate()
 
     // Serve static files
-    http.Handle("/", http.FileServer(http.Dir("/mnt/ramdisk/static")))
+    http.Handle("/", http.FileServer(http.Dir("./static")))
 
     // API endpoints
     http.HandleFunc("/api/system-info", handleSystemInfo)
