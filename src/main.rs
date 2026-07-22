b14738a33473c16ef90c20e2cae7aac8bf1b7e24
hiltar <43303122+hiltar@users.noreply.github.com>
2026-07-22 23:08:32 +0300

    Update logging macros to accept multiple arguments

--- src/main.rs
+++ src/main.rs
@@ -49,13 +49,13 @@ struct Assets;
 // NATIVE LOGGING MACROS
 // =============================================
 macro_rules! info {
-    ($($arg:tt)) => { println!("[INFO] {}", format!($($arg))) }
+    ($($arg:tt)*) => { println!("[INFO] {}", format!($($arg)*)) }
 }
 macro_rules! error {
-    ($($arg:tt)) => { eprintln!("[ERROR] {}", format!($($arg))) }
+    ($($arg:tt)*) => { eprintln!("[ERROR] {}", format!($($arg)*)) }
 }
 macro_rules! warn {
-    ($($arg:tt)) => { eprintln!("[WARN] {}", format!($($arg))) }
+    ($($arg:tt)*) => { eprintln!("[WARN] {}", format!($($arg)*)) }
 }
 
 // =============================================
